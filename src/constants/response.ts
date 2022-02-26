3ce79b5e24b8d51cffc9b567a174f95d932c4eb6
han <dev.ryu2021@gmail.com>
2022-02-27 01:01:13 +0900

    Create an app error handler

---
+++ src/constants/response.ts
@@ -0,0 +1,5 @@
+
+export const SUCCESS = { message: 'Success', status: 200 };
+export const NOT_FOUND = { message: 'Not Found', status: 404 };
+export const INTERNAL_SERVER_ERROR ={ message: 'Internal Server Error', status: 500 };
+
--- src/loaders/server.ts
+++ src/loaders/server.ts
@@ -1,20 +1,41 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import path from 'path';
 import { viewRouter, apiRouter } from '../routes';
 import { ENTRY, API } from '../constants/routes';
+import CustomError from '../services/errorHandler';
+import { NOT_FOUND } from '../constants';
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+try {
+  app.use(express.static('public'));
+  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-app.set( "views", path.join( __dirname, "../views" ) );
-app.set( "view engine", "ejs" );
+  app.set( "views", path.join( __dirname, "../views" ) );
+  app.set( "view engine", "ejs" );
 
-app.use(API, apiRouter);
-app.use(ENTRY, viewRouter);
+  app.use(API, apiRouter);
+  app.use(ENTRY, viewRouter);
+  app.use(() => {
+    throw new CustomError(NOT_FOUND);
+  });
+
+
+  app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+
+    if (process.env.NODE_ENV === 'development') {
+      console.error('Catch error in app error handelr', err.message);
+    }
+
+    res.status(err.status);
+    res.json(`An error occured with the code ${err.status}`);
+  });
+} catch (e) {
+  console.error('error occur during server.ts loading', e);
+}
+
 
 export default app;
