e7ec21688393b9e8d71a59d813763a0b023805e6
han <dev.ryu2021@gmail.com>
2022-03-09 22:24:36 +0900

    Check before insert file

--- src/controllers/dailychartControllers.ts
+++ src/controllers/dailychartControllers.ts
@@ -7,15 +7,36 @@ import { csvHandler, failResponse, CustomError, successResponse } from '../servi
 
 const Dailychart = db.models.dailychart;
 
+const getFindSyntax = (date: string): any => ({ where: { listDate: date } });
+
+
 const postDailychart = async (req: Request, res: Response) => {  
   
   const uploadDate = req.query.date as string;
-  const rowsForDB = csvHandler.parser(req.files, uploadDate);
+  const hasAlreadyUploadedList = await checkHasAlreadyUploadedList(uploadDate);
+
+  if (hasAlreadyUploadedList) {
+    await Dailychart.destroy(getFindSyntax(uploadDate));
+  }
+
+  const { result, message } = await insertUploadedFile(req.files, uploadDate);
+  const resultMessage = result ? successResponse(message) : failResponse(message);
+
+  res.status(SUCCESS.status).json(resultMessage);
+  return;
+};
+
+const checkHasAlreadyUploadedList = async (listDate: string) => {
+  const listOnUploadDate = await Dailychart.findOne(getFindSyntax(listDate));
+  return !_.isNil(listOnUploadDate);
+};
+
+const insertUploadedFile = async (files: Express.Request['files'], listDate: string) => {
+  const rowsForDB = csvHandler.parser(files, listDate);
 
   if (_.isEmpty(rowsForDB)) {
     console.log('fail to parse excel file');
-    res.status(SUCCESS.status).json(failResponse('fail to parse excel file'));
-    return;
+    return { result: false, messgae: 'fail to parse excel file' };
   }
   
   try {
@@ -27,20 +48,20 @@ const postDailychart = async (req: Request, res: Response) => {
       });
     }
 
-    res.status(SUCCESS.status).json(successResponse('success to upload'));
-    return; 
-  } catch (err: unknown) {
+    return { result: true, message: 'success to upload' }; 
+  } catch (err: any) {
     console.log('bulkCreate err in postSheet controller', err);
-    res.status(SUCCESS.status).json(failResponse(err));
+    return { result: false, message: err?.message };
   }
-};
+}
 
 const getDailychart = async (req: Request, res: Response) => {
   if (req.query.listDate) {
-    const targetByUploadDate = { where: { listDate: req.query.listDate } } as any;
+    
+    const targetByUploadDate = req.query.listDate as string;
 
     try {
-      const findResult = await Dailychart.findAll(targetByUploadDate);
+      const findResult = await Dailychart.findAll(getFindSyntax(targetByUploadDate));
       res.status(SUCCESS.status).json(findResult);
     } catch (err: unknown) {
       console.log('findAll err in postSheet controller', err);
